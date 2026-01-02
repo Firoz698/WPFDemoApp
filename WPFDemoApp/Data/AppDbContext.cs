3f8545d60748edc12984c2a438b62bc00b1c98c9
Firoz698 <mdfirozahmed698@gmail.com>
2026-01-02 23:58:49 +0600

    login page view design change

--- WPFDemoApp/Data/AppDbContext.cs
+++ WPFDemoApp/Data/AppDbContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,8 +15,7 @@ namespace WPFDemoApp.Data
 
         public AppDbContext()
         {
-            _connectionString = ConfigurationManager
-                .ConnectionStrings["DbConnection"].ConnectionString;
+            _connectionString = ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString;
         }
 
         public SqlConnection GetConnection()
@@ -23,4 +23,15 @@ namespace WPFDemoApp.Data
             return new SqlConnection(_connectionString);
         }
     }
+
+
+    //public class AppDbContext
+    //{
+    //    private readonly string _connectionString = "Server=DESKTOP-7TCVLOM\\SQLEXPRESS;Database=WPFDataBase;Trusted_Connection=True;TrustServerCertificate=True;";
+
+    //    public IDbConnection CreateConnection()
+    //    {
+    //        return new SqlConnection(_connectionString);
+    //    }
+    //}
 }
